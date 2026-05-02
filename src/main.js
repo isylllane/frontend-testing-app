e3728a5b1d69c4bdb4e1c84d23b787484ec50e4a
Isyllane <lutik301@gmail.com>
2026-05-02 14:00:19 +0300

    feat: настройка vuetify и основного файла main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+// Файл конфигурации маршрутов
+app.use(router)
+// Файл конфигурации vuetify
+app.use(vuetify)
+
+app.mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,30 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+export default createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: { mdi },
+    },
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    primary: '#2196F3',
+                    secondary: '#64B6F7',
+                    accent: '#2162ff',
+                    error: '#FF0000', //todo Согласовать цвет #FF5252
+                    success: '#0FC015', //todo Согласовать цвет #4CAF50
+                },
+            },
+        },
+    },
+})
